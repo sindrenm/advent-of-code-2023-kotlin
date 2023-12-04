245edbea665eb5c3771a1d53b893b8cbc8ec8323
Sindre Moen <sindrem@gmail.com>
2023-12-04 18:21:58 +0100

    Solve Day 03, part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -25,12 +25,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val gearRatios = mutableListOf<Int>()
+
+        input.mapIndexed { index, line ->
+            val previousLine = if (index == 0) null else input[index - 1]
+            val nextLine = if (index >= input.size - 1) null else input[index + 1]
+
+            val asteriskIndices = "(\\*)".toRegex()
+                .findAll(line).toList()
+                .map { it.range.first }
+
+            val summed = asteriskIndices.mapNotNull {
+                it.gearRatioOrNull(line, previousLine, nextLine)
+            }.sum()
+
+            gearRatios.add(summed)
+        }
+
+        return gearRatios.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 4361)
+    check(part2(testInput) == 467835)
 
     val input = readInput("Day03")
     part1(input).println()
@@ -66,3 +83,41 @@ fun Char.isSymbol(): Boolean {
 
     return true
 }
+
+fun Int.gearRatioOrNull(thisLine: String, previousLine: String?, nextLine: String?): Int? {
+    val startIndex = minus(1).coerceAtLeast(0)
+    val endIndex = plus(1).coerceAtMost(thisLine.length - 1)
+    val requiredRange = IntRange(startIndex, endIndex)
+
+    val allAdjacent = listOfNotNull(
+        previousLine.numbersInRange(requiredRange),
+        thisLine.numbersInRange(requiredRange),
+        nextLine.numbersInRange(requiredRange),
+    ).flatten()
+
+    if (allAdjacent.count() < 2) return null
+
+    return allAdjacent.reduce(Int::times)
+}
+
+fun String?.numbersInRange(range: IntRange): List<Int>? {
+    if (this.isNullOrBlank()) return null
+
+    return this
+        .let(numberRegex::findAll).toList()
+        .flatMap { it.groups }
+        .filterNotNull()
+        .filter { it.range in range }
+        .distinct()
+        .map { it.value.toInt() }
+        .takeUnless { it.isEmpty() }
+}
+
+operator fun IntRange.contains(other: IntRange): Boolean {
+    if (other.first in this) return true
+    if (other.last in this) return true
+
+    return false
+}
+
+val numberRegex = "(\\d+)".toRegex()
