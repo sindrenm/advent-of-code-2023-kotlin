7624d2ae7dc4cd50f9513b6915292d83aa9f0de5
Sindre Moen <sindrenm@gmail.com>
2023-12-04 08:27:00 +0100

    Solve Day 01, part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -10,14 +10,45 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.stringDigitToDigit() }
+            .sumOf { line ->
+                val firstDigit = line.first { it.isDigit() }
+                val lastDigit = line.last { it.isDigit() }
+
+                "$firstDigit$lastDigit".toInt()
+            }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+fun String.stringDigitToDigit(): String {
+    return replace(stringDigitsMatcher.toRegex()) { matchResult ->
+        stringDigits[matchResult.value].toString()
+    }
+}
+
+val stringDigits = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+)
+
+val stringDigitsMatcher = buildString {
+    append("(")
+    append(stringDigits.keys.joinToString("|"))
+    append(")")
 }
