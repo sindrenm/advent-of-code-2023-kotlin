47e8c5e36b18aaf9dc55d54d5729c3d759bc3013
Sindre Moen <sindrenm@gmail.com>
2023-12-04 08:25:18 +0100

    Solve Day 01, part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,12 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .sumOf { line ->
+                val firstDigit = line.first { it.isDigit() }
+                val lastDigit = line.last { it.isDigit() }
+
+                "$firstDigit$lastDigit".toInt()
+            }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +15,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
